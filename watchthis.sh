5a22bbdd2148eae51e2b3f19208429d9f05009f9
Jay Goldberg <jaymgoldberg@gmail.com>
2015-11-19 19:43:02 -0800

    added functionality
    Add a timeout option
    use curl's print output formatting
    use curl's -o output redirection
    use curl -s silent option
    email address pulled out

--- watchthis.sh
+++ watchthis.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 HOSTS="www.example.com 123.456.78.910"
+TIMEOUT=6
+EMAIL="your.email@example.com"
 
 for hostname in $HOSTS
 do
-	status_line=`curl -iL $hostname 2>/dev/null | head -n 1`
-	status_code=`echo $status_line | awk '{print $2}'`
-	echo "${status_code}"
-	if [ $status_code -ne 200 ]
-		then echo "Error: $status_code on $hostname"
-		echo "Error: $status_code" | mail -s "watchthis: $hostname is down" -t "your.email@example.com" < /dev/null
-	fi
+  status_code=$(curl -sL -w '%{http_code}' $hostname -m $TIMEOUT -o /dev/null)
+  echo "$status_code"
+  if [ "$status_code" -ne 200 ]
+    then echo "Error: $status_code on $hostname"
+    echo "Error: $status_code" | mail -s "watchthis: $hostname is down" -t "$EMAIL" < /dev/null
+  fi
 done
